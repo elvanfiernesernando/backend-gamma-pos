c0a799a43a1de7f7917512fa53b71ae152e293b0
Elvan Fiernes Ernando <elvanfiernesernando@gmail.com>
2022-10-23 09:32:22 +0700

    Add get category by id endpoint

--- app/categories/controllers/CategoryController.js
+++ app/categories/controllers/CategoryController.js
@@ -26,7 +26,11 @@ const category_create = async (req = request, res = response) => {
 // get all categories
 const category_all = async (req = request, res = response) => {
     try {
-        const categoryData = await db.categories.findMany()
+        const categoryData = await db.categories.findMany({
+            include: {
+                products: true
+            }
+        })
 
         return res.status(200).json({
             status: 200,
@@ -39,4 +43,29 @@ const category_all = async (req = request, res = response) => {
 
 }
 
-module.exports = { category_create, category_all };
+// get category by id
+const category_by_id = async (req = request, res = response) => {
+    try {
+
+        const id = parseInt(req.params.id);
+
+        const categoryData = await db.categories.findUnique({
+            where: {
+                id: id
+            },
+            include: {
+                products: true
+            }
+        })
+
+        return res.status(200).json({
+            status: 200,
+            message: "OK",
+            data: categoryData
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = { category_create, category_all, category_by_id };
--- app/categories/routes/CategoryRoute.js
+++ app/categories/routes/CategoryRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { category_create, category_all } = require("../controllers/CategoryController");
+const { category_create, category_all, category_by_id } = require("../controllers/CategoryController");
 
 // import controller
 const category_route = express.Router();
 
 category_route.post("/api/categories", category_create);
 category_route.get("/api/categories", category_all);
-category_route.get("/api/categories/:id", category_all);
+category_route.get("/api/categories/:id", category_by_id);
 
 module.exports = category_route;
